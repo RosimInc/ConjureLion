1921e3297eeb7671107e3a71d99bc2bffab97456
Yvanovitch <yvan.richer@gmail.com>
2015-03-20 01:06:52 -0400

    MAJ test rail Ajout Souffli

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public BezierSpline spline;
+	public float speed = 1f;
+	public float defaultProgress = 0f;
+	public GameObject fix;
+	private float progress;
+	
+	// Use this for initialization
+	void Start () {
+		Vector3 position = spline.GetPoint(progress);
+		transform.localPosition = position;
+	}
+	
+	private void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			progress += Time.deltaTime * speed;
+			if (progress > 1f) {
+					progress = 1f;
+			}
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow) ) {
+			progress -= Time.deltaTime * speed;
+			if (progress < 0f) {
+				progress = 0f;
+			}
+		}
+		
+		Vector3 position = spline.GetPoint(progress);
+		transform.localPosition = position;
+		
+		//Orientation du fix
+		fix.transform.LookAt(position + spline.GetDirection(progress) );
+		fix.transform.Rotate(new Vector3(0,90,0));
+	}
+}
