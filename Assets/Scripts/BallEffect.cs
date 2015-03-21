86be9e8cca3dc70a572e128c1c3ee1d89e096b2b
Yvanovitch <yvan.richer@gmail.com>
2015-03-21 00:15:59 -0400

    Début de gestion du souffle dans les tuyau par joueur

--- Assets/Scripts/BallEffect.cs
+++ Assets/Scripts/BallEffect.cs
@@ -9,7 +9,7 @@ public class BallEffect : MonoBehaviour {
 	public float vision;
 	public bool attract;
 	public GameObject aim;
-	public Ball ball;
+	private Ball ball;
 	public bool activated;
     public GameObject staticDetection;
     public int PlayerNumber = 1;
@@ -106,6 +106,55 @@ public class BallEffect : MonoBehaviour {
 			ball.rigidbody2D.AddForce(force * mod * toBall.normalized *
 					(radius - distance) / radius * Time.fixedDeltaTime *
                     maxTriggerValue, ForceMode2D.Force);
+		}
+	}
+	
+	private void checkTouchPipe(GameObject obj)
+	{
+		if (obj == null) return;
+		
+		Vector3 toBall = obj.transform.position - transform.position;
+		float distance = toBall.magnitude;
+		
+		//Out of ranges
+		if (distance > radius) return;
+		
+		float angle = Vector3.Angle(aim.transform.forward,
+		                            obj.transform.position - aim.transform.position);
+		
+		Vector3 local =
+			aim.transform.InverseTransformPoint(obj.transform.position);
+		
+		if (local.x < 0)
+			angle = -angle;
+		//Angle between 0 (right) and 180 (left) and 90 (up) and -90 (down)
+		
+		if (angle > 180)
+			angle = -(360 - angle);
+		//Angle between 0 (up) and 180 (down) and -90 (left) and 90 (right)
+		
+		if (angle * angle > vision * vision) return;
+		
+		
+		int mod = attract ? -1 : 1;
+		
+		//Determine if there's no shadowing obstacle in the way
+		RaycastHit hit;
+		
+		Debug.DrawRay(transform.position, (obj.transform.position - transform.position));
+		//1 << 8 - 1 << 10
+		
+		if (Physics.Raycast(transform.position, (obj.transform.position - transform.position), out hit,
+		                    radius))
+		{
+			if (hit.collider.gameObject.layer == 8) return;
+			
+			float maxTriggerValue = Mathf.Max( Input.GetAxisRaw("TriggersL_" + PlayerNumber), Input.GetAxisRaw("TriggersR_" + PlayerNumber));
+			maxTriggerValue = Mathf.Max ( maxTriggerValue, Math.Abs(Input.GetAxisRaw("TriggersLR_" + PlayerNumber)));
+			
+			ball.rigidbody2D.AddForce(force * mod * toBall.normalized *
+			                          (radius - distance) / radius * Time.fixedDeltaTime *
+			                          maxTriggerValue, ForceMode2D.Force);
 		}
 	}
 
