a92ab3c7c7394785d3bdcf5c43a3aa8159a010bc
MaDumont <dumont.marcantoine13@gmail.com>
2015-04-24 19:41:03 -0400

    - Added the blinking eye animation for Aspi, but there's a problem with the png of the new body (thin black border on one of the borders)

---
+++ Assets/Scripts/BlinkingEye.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkingEye : MonoBehaviour
+{
+	public Transform PaupiereHaut;
+	public Transform PaupiereBas;
+
+	public Vector3 PaupiereHautOpenPos;
+	public Vector3 PaupiereHautClosedPos;
+
+	public Vector3 PaupiereBasOpenPos;
+	public Vector3 PaupiereBasClosedPos;
+
+	private bool _isBlinking = false;
+
+	void Update ()
+	{
+		if (UnityEngine.Random.Range(0f, 1f) < 0.005f && !_isBlinking)
+		{
+			StartCoroutine(Blink());
+		}
+	}
+
+	private IEnumerator Blink()
+	{
+		_isBlinking = true;
+
+		float ratio = 0f;
+
+		while (ratio < 1f)
+		{
+			ratio += Time.deltaTime / 0.1f;
+
+			PaupiereHaut.localPosition = Vector3.Lerp(PaupiereHautOpenPos, PaupiereHautClosedPos, ratio);
+			PaupiereBas.localPosition = Vector3.Lerp(PaupiereBasOpenPos, PaupiereBasClosedPos, ratio);
+
+			yield return null;
+		}
+
+		ratio = 0f;
+
+		while (ratio < 1f)
+		{
+			ratio += Time.deltaTime / 0.1f;
+
+			PaupiereHaut.localPosition = Vector3.Lerp(PaupiereHautClosedPos, PaupiereHautOpenPos, ratio);
+			PaupiereBas.localPosition = Vector3.Lerp(PaupiereBasClosedPos, PaupiereBasOpenPos, ratio);
+
+			yield return null;
+		}
+
+		_isBlinking = false;
+	}
+}
--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -18,11 +18,12 @@ public abstract class Robot : MonoBehaviour
 
 	private float _targetAngle;
 
-    protected Player _player;
+	protected Player _player;
+	
 
 	void Awake()
 	{
-        _player = GetComponent<Player>();
+		_player = GetComponent<Player>();
 
 		_previousParticlesVelocity = WindParticles.startSpeed;
 		_targetAngle = Body.localEulerAngles.z;
@@ -38,18 +39,18 @@ public abstract class Robot : MonoBehaviour
 
 	protected virtual void Update()
 	{
-        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[WindParticles.particleCount];
+		ParticleSystem.Particle[] particles = new ParticleSystem.Particle[WindParticles.particleCount];
 
-        int num = WindParticles.GetParticles(particles);
+		int num = WindParticles.GetParticles(particles);
 
-        for (int i = 0; i < num; i++)
-        {
-            particles[i].lifetime = 0;
-        }
+		for (int i = 0; i < num; i++)
+		{
+			particles[i].lifetime = 0;
+		}
 
-        //WindParticles.SetParticles(particles, num);
+		//WindParticles.SetParticles(particles, num);
 
-        //Debug.Log(num);
+		//Debug.Log(num);
 
 		if (Body.localEulerAngles.z < 0f)
 		{
@@ -68,11 +69,11 @@ public abstract class Robot : MonoBehaviour
 			Body.localEulerAngles = new Vector3(0f, 0f, newAngle);
 		}
 
-        float maxTriggerValue = InputManager.Instance.GetInputBreathAction(_player.Number);
+		float maxTriggerValue = InputManager.Instance.GetInputBreathAction(_player.Number);
 
-        if (InputManager.Instance.GetInputRotation(_player.Number).magnitude != 0f)
+		if (InputManager.Instance.GetInputRotation(_player.Number).magnitude != 0f)
 		{
-            _targetAngle = -(Mathf.Atan2(InputManager.Instance.GetInputRotation(_player.Number).x, InputManager.Instance.GetInputRotation(_player.Number).y) * Mathf.Rad2Deg);
+			_targetAngle = -(Mathf.Atan2(InputManager.Instance.GetInputRotation(_player.Number).x, InputManager.Instance.GetInputRotation(_player.Number).y) * Mathf.Rad2Deg);
 		}
 
 		if (maxTriggerValue > 0f)
@@ -103,62 +104,62 @@ public abstract class Robot : MonoBehaviour
 		if(_ballEffect == null)
 			_ballEffect = GetComponent<BallEffect>();
 
-        if (_ballEffect != null)
-        {
-            _ballEffect.activated = state;
-        }
+		if (_ballEffect != null)
+		{
+			_ballEffect.activated = state;
+		}
 
 		if (state)
 		{
 			WindParticles.Play();
 
-            if (this is Souffli)
-            {
-                MusicManager.Instance.PlaySouffliBreathIn();
-                StopCoroutine("PlaySouffliBreathLoop");
-                StartCoroutine("PlaySouffliBreathLoop");
-            }
-            else
-            {
-                MusicManager.Instance.PlayAspiBreathIn();
-                StopCoroutine("PlayAspiBreathLoop");
-                StartCoroutine("PlayAspiBreathLoop");
-            }
+			if (this is Souffli)
+			{
+				MusicManager.Instance.PlaySouffliBreathIn();
+				StopCoroutine("PlaySouffliBreathLoop");
+				StartCoroutine("PlaySouffliBreathLoop");
+			}
+			else
+			{
+				MusicManager.Instance.PlayAspiBreathIn();
+				StopCoroutine("PlayAspiBreathLoop");
+				StartCoroutine("PlayAspiBreathLoop");
+			}
 		}
 		else
 		{
-            if (this is Souffli)
-            {
-                MusicManager.Instance.StopSouffliBreathLoop();
-                MusicManager.Instance.StopSouffliBreathIn();
-            }
-            else
-            {
-                MusicManager.Instance.StopAspiBreathLoop();
-                MusicManager.Instance.StopAspiBreathIn();
-            }
+			if (this is Souffli)
+			{
+				MusicManager.Instance.StopSouffliBreathLoop();
+				MusicManager.Instance.StopSouffliBreathIn();
+			}
+			else
+			{
+				MusicManager.Instance.StopAspiBreathLoop();
+				MusicManager.Instance.StopAspiBreathIn();
+			}
 
 			WindParticles.Stop();
 		}
 	}
 
-    private IEnumerator PlaySouffliBreathLoop()
-    {
-        yield return new WaitForSeconds(MusicManager.Instance.SouffliBreathIn.time);
-        
-        if (_isActivated)
-        {
-            MusicManager.Instance.PlaySouffliBreathLoop();
-        }
-    }
+	private IEnumerator PlaySouffliBreathLoop()
+	{
+		yield return new WaitForSeconds(MusicManager.Instance.SouffliBreathIn.time);
+		
+		if (_isActivated)
+		{
+			MusicManager.Instance.PlaySouffliBreathLoop();
+		}
+	}
 
-    private IEnumerator PlayAspiBreathLoop()
-    {
-        yield return new WaitForSeconds(MusicManager.Instance.AspiBreathIn.time);
+	private IEnumerator PlayAspiBreathLoop()
+	{
+		yield return new WaitForSeconds(MusicManager.Instance.AspiBreathIn.time);
 
-        if (_isActivated)
-        {
-            MusicManager.Instance.PlayAspiBreathLoop();
-        }
-    }
+		if (_isActivated)
+		{
+			MusicManager.Instance.PlayAspiBreathLoop();
+		}
+	}
 }
